0aca9ffaa92c09f67c6683e90a61f4397d4da76a
Kensuke Hoshikawa <kensuke1751@gmail.com>
2014-12-13 01:51:43 +0900

    add firealpaca

---
+++ Casks/firealpaca.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'firealpaca' do
+  version '1.1.12'
+  sha256 :no_check
+
+  url 'http://firealpaca.com/download.php?os=mac&key=17813449013210197561d4f66c5aca8c'
+  homepage 'http://firealpaca.com/'
+  license :unknown
+
+  app 'FireAlpaca.app'
+end
