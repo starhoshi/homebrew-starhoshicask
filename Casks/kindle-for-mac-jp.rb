f41c03b8fe9bc7f925d1f7e6d3734225d875125f
Kensuke Hoshikawa <kensuke1751@gmail.com>
2015-02-14 15:34:21 +0900

    add KindleForMac

---
+++ Casks/kindle-for-mac-jp.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'firealpaca' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.amazon.co.jp/kindlemacdownload'
+  homepage 'http://www.amazon.co.jp/gp/feature.html?ie=UTF8&docId=3077089416'
+  license :unknown
+
+  app 'Kindle.app'
+end
